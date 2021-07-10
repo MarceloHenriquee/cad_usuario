60d1c44bf6a4698e3d332ff152fb763d1faa23ea
Marcelo Henrique <marcelohphn@gmail.com>
2021-07-09 23:19:01 -0300

    Criação migração Clients

---
+++ database/migrations/2021_07_10_002724_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->date('date_birth')->nullable()->format('d-m-Y');
+            $table->string('password')->min_length(8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
