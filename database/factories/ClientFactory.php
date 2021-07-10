195e5501e14e222012fbf1e741a935850fc88a58
Marcelo Henrique <marcelohphn@gmail.com>
2021-07-09 23:42:15 -0300

    criação factory client

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Client;
+use Faker\Generator as Faker;
+
+$factory->define(Client::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'date_birth' => $faker->date(),
+        'password' => $faker->password
+    ];
+});
